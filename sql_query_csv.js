5bf722ec90f13fe583ffc52fd8ebc57a573f9496
Matt H. <matt_henderson@sil.org>
2026-01-27 10:30:39 -0500

    Port over SQL Query CSV from playground project where it was developed

---
+++ sql_query_csv.js
@@ -0,0 +1,43 @@
+import alasql from 'alasql'
+import Papa from 'papaparse'
+
+export default defineComponent({
+  name: "SQL Query CSV",
+  description: "Use a SQL query to get data from CSV data",
+  key: "sql_query_csv",
+  version: "0.0.1",
+  type: "action",
+
+  props: {
+    csv_content: {
+      type: "string",
+      label: "CSV content",
+      description: "The CSV content (string) to run the SQL query against"
+    },
+    csv_has_header: {
+      type: "boolean",
+      label: "CSV has header row?",
+      description: "Whether the CSV data has a header row. If so, the data rows' data will be keyed on field names instead of indexes.",
+      default: true,
+    },
+    sql_query: {
+      type: "string",
+      label: "SQL query",
+      description: "The SQL query to run against the provided CSV data. Use a question mark (?) for the table name.",
+    },
+  },
+  async run({ steps, $ }) {
+    const csvParseResults = Papa.parse(
+      this.csv_content,
+      {
+        header: this.csv_has_header,
+        skipEmptyLines: true,
+      }
+    )
+    const rows = alasql(
+      this.sql_query,
+      [csvParseResults.data]
+    )
+    return { rows }
+  },
+})
