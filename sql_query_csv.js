38ee7c9252067fccb26b591195fba9e335c3b1de
Matt H. <matt_henderson@sil.org>
2026-01-27 14:51:47 -0500

    Bump SQL Query CSV version

--- sql_query_csv.js
+++ sql_query_csv.js
@@ -5,7 +5,7 @@ export default defineComponent({
   name: "SQL Query CSV",
   description: "Use a SQL query to get data from CSV data",
   key: "sql_query_csv",
-  version: "0.0.1",
+  version: "0.0.2",
   type: "action",
 
   props: {
