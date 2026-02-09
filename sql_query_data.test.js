fd551c2ad6ac1e615b66a8ba32e3f97f6c911d48
Matt H. <matt_henderson@sil.org>
2026-02-09 13:51:45 -0500

    Fix data_inputs for sql_query_data test
    Extracting it to a separate variable made it clearer why another level
    of array was needed.

--- sql_query_data.test.js
+++ sql_query_data.test.js
@@ -3,20 +3,22 @@ import { describe, it } from 'node:test'
 
 const { default: component } = await import('./sql_query_data.js')
 
+const books = [
+  {
+    "Title": "The Hobbit",
+    "Genre": "Fantasy",
+    "Author": "J.R.R. Tolkien",
+  },
+  {
+    "Title": "Pride and Prejudice",
+    "Genre": "Fiction",
+    "Author": "Jane Austen",
+  },
+]
+
 describe(component.name, () => {
   it('should retrieve the specified row of data', async () => {
-    component.data_inputs = [
-      {
-        "Title": "The Hobbit",
-        "Genre": "Fantasy",
-        "Author": "J.R.R. Tolkien",
-      },
-      {
-        "Title": "Pride and Prejudice",
-        "Genre": "Fiction",
-        "Author": "Jane Austen",
-      },
-    ]
+    component.data_inputs = [books]
     component.sql_query = 'SELECT Title FROM ? WHERE Genre = "Fiction"'
 
     const response = await component.run({
