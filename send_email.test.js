cfa7f5aac45e7aa3c773d697c1da0c1a0ec13b27
Matt H. <matt_henderson@sil.org>
2026-02-10 14:31:54 -0500

    Add test for sending an email with an attachment

--- send_email.test.js
+++ send_email.test.js
@@ -44,4 +44,52 @@ describe(component.name, () => {
 
     assert.ok(response.MessageId, 'No MessageId was returned')
   })
+
+  it('should be able to send an email with an attachment', async (testContext) => {
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+    if (!accessKeyId) {
+      testContext.skip('No `AWS_ACCESS_KEY_ID` env. var. provided, skipping email test')
+      return
+    }
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+    if (!secretAccessKey) {
+      testContext.skip('No `AWS_SECRET_ACCESS_KEY` env. var. provided, skipping email test')
+      return
+    }
+    if (!process.env.EMAIL_TEST_ATTACHMENT) {
+      testContext.skip('No `EMAIL_TEST_ATTACHMENT` was provided, so skipping "with attachment" test')
+      return
+    }
+    component.amazon_ses = {
+      "$auth": { accessKeyId, secretAccessKey },
+    }
+
+    component.to = process.env.EMAIL_TEST_TO
+    assert.ok(component.to, 'No "To" address provided')
+
+    component.from = process.env.EMAIL_TEST_FROM
+    assert.ok(component.from, 'No "From" address provided')
+
+    component.subject = process.env.EMAIL_TEST_SUBJECT
+    assert.ok(component.subject, 'No email subject provided')
+
+    component.body = process.env.EMAIL_TEST_BODY
+    assert.ok(component.body, 'No email body provided')
+
+    component.attachmentFilename = ['example.csv']
+    component.attachmentContent = [
+      `Title,Genre,Author
+"The Hobbit",Fantasy,"J.R.R. Tolkien"
+"Pride and Prejudice",Fiction,"Jane Austen"`
+    ]
+    component.attachmentType = ['text/csv']
+    component.attachmentEncoding = 'BASE64'
+
+    const response = await component.run({
+      steps: { trigger: {} },
+      $: {}
+    })
+
+    assert.ok(response.MessageId, 'No MessageId was returned')
+  })
 })
