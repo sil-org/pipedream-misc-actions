bcd57c105da85a2c24e72d57680cfeff4e888635
Matt H. <matt_henderson@sil.org>
2026-03-31 15:14:44 -0400

    Increment the existing value in the cell (if present)

--- update_metric_in_google_sheet.js
+++ update_metric_in_google_sheet.js
@@ -13,6 +13,11 @@ export default {
       label: "File Name",
       description: "The name of the file being processed"
     },
+    record_type: {
+      type: "string",
+      label: "Record Type",
+      description: "The type of the record being processed"
+    },
     google_sheet_id: {
       type: "string",
       label: "Google Sheet ID",
@@ -27,20 +32,38 @@ export default {
   async run({ steps, $ }) {
     return await updateMetric(
       this.source_file_name,
+      this.record_type,
       this.google_sheet_id,
       this.google_service_account_key,
     )
   },
 }
 
-const updateMetric = async (sourceFileName, googleSheetId, googleServiceAccountKey) => {
+const updateMetric = async (
+  sourceFileName,
+  recordType,
+  googleSheetId,
+  googleServiceAccountKey
+) => {
   const auth = new google.auth.GoogleAuth({
     credentials: JSON.parse(googleServiceAccountKey),
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   })
   const sheets = google.sheets({ version: 'v4', auth })
 
-  // TODO: Find which column is for the type of record being processed.
+  // Find which column is for the type of record being processed.
+  const headerRes = await sheets.spreadsheets.values.get({
+    spreadsheetId: googleSheetId,
+    range: '1:1',
+  })
+  const headers = (headerRes.data.values || [])[0] || []
+  let colIndex = headers.indexOf(recordType)
+
+  if (colIndex === -1) {
+    return {
+      error: `No column found for record type: ${recordType}`
+    }
+  }
 
   // See if there is an existing row for this file
   const res = await sheets.spreadsheets.values.get({
@@ -53,22 +76,49 @@ const updateMetric = async (sourceFileName, googleSheetId, googleServiceAccountK
   // Either insert a new row or update the existing row to count this record.
   let insertedNewRow = false
   if (!foundRow) {
+    const values = new Array(colIndex + 1).fill("")
+    values[0] = sourceFileName
+    values[colIndex] = 1
     await sheets.spreadsheets.values.append({
       spreadsheetId: googleSheetId,
-      range: 'A:A',
+      range: 'A1',
       valueInputOption: 'USER_ENTERED',
       resource: {
-        values: [[sourceFileName]]
+        values: [values]
       }
     })
     insertedNewRow = true
   } else {
-    // TODO: Update the existing cell
+    const rowIndex = rows.indexOf(foundRow) + 1
+    const getColumnLetter = (index) => {
+      let letter = ''
+      while (index >= 0) {
+        letter = String.fromCharCode((index % 26) + 65) + letter
+        index = Math.floor(index / 26) - 1
+      }
+      return letter
+    }
+    const colLetter = getColumnLetter(colIndex)
+    const cellRange = `${colLetter}${rowIndex}`
+    
+    const cellRes = await sheets.spreadsheets.values.get({
+      spreadsheetId: googleSheetId,
+      range: cellRange,
+    })
+    const currentVal = parseInt((cellRes.data.values || [[]])[0][0] || 0)
+    
+    await sheets.spreadsheets.values.update({
+      spreadsheetId: googleSheetId,
+      range: cellRange,
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: [[currentVal + 1]]
+      }
+    })
   }
 
   return {
     insertedNewRow,
-    // ...
   }
 }
 
