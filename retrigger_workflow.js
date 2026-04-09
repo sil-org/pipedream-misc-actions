1ce8239bd11564460940b531a830afe3bb2e9f51
Jason Jackson <jasonc.jackson@gmail.com>
2026-04-08 21:12:44 -0400

    bump to v0.1.0

--- retrigger_workflow.js
+++ retrigger_workflow.js
@@ -4,7 +4,7 @@ export default {
   description:
     "Start a recursive call to the same workflow, passing in the datastore that will be used to determine whether another call is needed.",
   key: "retrigger_workflow",
-  version: "0.0.2",
+  version: "0.1.0",
   type: "action",
 
   props: {
