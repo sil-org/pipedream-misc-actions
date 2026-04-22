387ea1303d5fcc3b387ecf35c2e07e5024b1f304
Matt H. <matt_henderson@sil.org>
2026-04-22 11:56:07 -0400

    Adjust wording of step description

--- is_prod_subworkflow.js
+++ is_prod_subworkflow.js
@@ -1,6 +1,6 @@
 export default {
   name: "Is Production? (Sub-workflows)",
-  description: "Safely determine whether we should run in production or develop mode.",
+  description: "Safely determine whether this sub-workflow should run in production or develop mode.",
   key: "is-prod-subworkflow",
   version: "0.1.0",
   type: "action",
