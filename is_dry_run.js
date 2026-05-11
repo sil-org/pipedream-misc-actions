98befb290cf2f2c925e8af6dd511dd84bd1966fc
Matt H. <matt_henderson@sil.org>
2026-05-11 12:28:22 -0400

    Change "Is Dry Run? (Dispatcher)" to separately control dev/prod dry-run

--- is_dry_run.js
+++ is_dry_run.js
@@ -1,20 +1,32 @@
 export default {
   name: "Is Dry Run? (Dispatcher)",
-  description: "Toggle whether to run this workflow as a dry-run",
+  description: "Toggle whether to run this workflow as a dry-run. Use the output of this in any steps you want to have act differently in dry-run mode.",
   key: "is-dry-run",
-  version: "0.1.1",
+  version: "0.2.0",
   type: "action",
 
   props: {
-    is_dry_run: {
-      label: "Is dry run?",
-      description: "A toggle for whether to do a dry-run. Use the output of this in any steps you want to act differently in dry-run mode.",
+    is_prod: {
+      label: "Is production?",
+      description: "Whether the workflow is running in production or development mode. This should usually be the output of an 'Is Production?' step.",
+      type: "boolean",
+    },
+    is_dev_dry_run: {
+      label: "Use dry-run for DEV?",
+      description: "Toggle for whether DEV runs should be in dry-run mode.",
+      type: "boolean",
+    },
+    is_prod_dry_run: {
+      label: "Use dry-run for PROD?",
+      description: "Toggle for whether PROD runs should be in dry-run mode.",
       type: "boolean",
     }
   },
 
   async run() {
-    console.log('Is dry run?', this.is_dry_run)
-    return this.is_dry_run
+    const isDryRun = this.is_prod ? this.is_prod_dry_run : this.is_dev_dry_run
+    console.log('Is production?', this.is_prod)
+    console.log('Is dry run?', isDryRun)
+    return isDryRun
   },
 }
